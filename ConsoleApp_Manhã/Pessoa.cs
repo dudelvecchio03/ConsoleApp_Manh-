59ac896b308d80425bad1d37ca9e381092be0701
dudelvecchio23 <dudelvecchio03@gmail.com>
2023-03-08 10:21:50 -0300

    Criação da classe pessoa, set e get

---
+++ ConsoleApp_Manhã/Pessoa.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Manhã
+{
+    public class Pessoa
+    {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+        public Pessoa(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void setId(int id)
+        {
+            if (id > 0)
+            {
+                Id = id;
+            }
+            Id = id;
+        }
+
+
+        public string getNome()
+        {
+            return Nome;
+        } 
+
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+
+
+    }
+
+
+}
