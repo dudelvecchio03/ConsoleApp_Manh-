04172549e9068832af10c730c3ff9b2f4a349c76
dudelvecchio23 <dudelvecchio03@gmail.com>
2023-03-08 10:31:56 -0300

    Alteração no console

--- ConsoleApp_Manhã/Program.cs
+++ ConsoleApp_Manhã/Program.cs
@@ -6,4 +6,5 @@ Console.WriteLine("Hello, World!");
 //Produto p1 = new Produto(1, "Mouse sem fio", 59.90m);
 //Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
 
-
+Pessoa pessoa1 = new Pessoa(1, "Eduardo Del Vecchio", "993213904");
+Console.WriteLine("Nome:" + pessoa1.getNome() + " Telefone:" + pessoa1.getTelefone());
