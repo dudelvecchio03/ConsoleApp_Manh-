5d77d1006c66255cf4c694a19c946c76d9d237d1
dudelvecchio23 <dudelvecchio03@gmail.com>
2023-03-15 08:28:47 -0300

    Classe cliente - Classe Endereço

---
+++ ConsoleApp_Manhã/Endereco.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Manhã
+{
+    public class Endereco
+    {
+        private string Rua;
+        private string Numero;
+        private string CEP;
+        private string Bairro;
+        private string Cidade;
+        private string Estado;
+
+
+
+        public Endereco(string rua, string numero, string cep, string bairro, string cidade, string estado)
+        {
+            Rua = rua;
+            Numero = numero;
+            CEP = cep;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+        }
+
+        
+        public void setRua(string rua)
+        {
+            Rua = rua;
+        }
+
+        public string getRua()
+        {
+            return Rua;
+        }
+
+        public void setNumero(string numero)
+        {
+            Numero = numero;
+        }
+
+        public string getNumero()
+        {
+            return Numero;
+        }
+
+        public void setCEP(string cep)
+        {
+            CEP = cep;
+        }
+
+        public string getCEP()
+        {
+            return CEP;
+        }
+
+        public void setBairro(string bairro)
+        {
+            Bairro = bairro;
+        }
+
+        public string getBairro()
+        {
+            return Bairro;
+        }
+
+        public void setCidade(string cidade)
+        {
+            Cidade = cidade;
+        }
+
+        public string cidade()
+        {
+            return Cidade;
+        }
+
+        public void setEstado(string estado)
+        {
+            Estado = estado;
+        }
+
+        public string getEstado()
+        {
+            return Estado;
+        }
+    }
+
+}
--- ConsoleApp_Manhã/Program.cs
+++ ConsoleApp_Manhã/Program.cs
@@ -6,5 +6,7 @@ Console.WriteLine("Hello, World!");
 //Produto p1 = new Produto(1, "Mouse sem fio", 59.90m);
 //Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
 
+// !! Set = alterar valor de um objeto !!
+
 Pessoa pessoa1 = new Pessoa(1, "Eduardo Del Vecchio", "993213904");
 Console.WriteLine("Nome:" + pessoa1.getNome() + " Telefone:" + pessoa1.getTelefone());
